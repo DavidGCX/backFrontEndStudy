4c68354154e053790bf78f501a97bdcee81be435
DGCX <c62gao@uwaterloo.ca>
2024-03-01 10:52:34 -0500

    set production email

--- utils/email.js
+++ utils/email.js
@@ -1,56 +1,53 @@
-const { htmlToText } = require('html-to-text');
-const nodemailer = require('nodemailer');
-const pug = require('pug');
+const { htmlToText } = require("html-to-text");
+const nodemailer = require("nodemailer");
+const pug = require("pug");
 
 module.exports = class Email {
-	constructor(user, url) {
-		this.to = user.email;
-		this.firstName = user.name.split(' ')[0];
-		this.url = url;
-		this.from = `WebEmailTest <${process.env.EMAIL_FROM}>`;
-	}
+  constructor(user, url) {
+    this.to = user.email;
+    this.firstName = user.name.split(" ")[0];
+    this.url = url;
+    this.from = `WebEmailTest <${process.env.EMAIL_FROM}>`;
+  }
 
-	newTransport() {
-		if (process.env.NODE_ENV === 'production') {
-			return 1;
-		}
-		return nodemailer.createTransport({
-			host: process.env.EMAIL_HOST,
-			port: process.env.EMAIL_PORT,
-			auth: {
-				user: process.env.EMAIL_USERNAME,
-				pass: process.env.EMAIL_PASSWORD,
-			},
-		});
-	}
+  newTransport() {
+    return nodemailer.createTransport({
+      host: process.env.EMAIL_HOST,
+      port: process.env.EMAIL_PORT,
+      auth: {
+        user: process.env.EMAIL_USERNAME,
+        pass: process.env.EMAIL_PASSWORD,
+      },
+    });
+  }
 
-	async send(template, subject) {
-		const html = pug.renderFile(`${__dirname}/../views/email/${template}.pug`, {
-			firstName: this.firstName,
-			url: this.url,
-			subject,
-		});
+  async send(template, subject) {
+    const html = pug.renderFile(`${__dirname}/../views/email/${template}.pug`, {
+      firstName: this.firstName,
+      url: this.url,
+      subject,
+    });
 
-		const mailOptions = {
-			from: this.from,
-			to: this.to,
-			subject,
-			html,
-			text: htmlToText(html),
-		};
+    const mailOptions = {
+      from: this.from,
+      to: this.to,
+      subject,
+      html,
+      text: htmlToText(html),
+    };
 
-		await this.newTransport().sendMail(mailOptions);
-	}
+    await this.newTransport().sendMail(mailOptions);
+  }
 
-	// Send the welcome email
-	async sendWelcome() {
-		await this.send('welcome', 'Welcome to the Natours Family!');
-	}
+  // Send the welcome email
+  async sendWelcome() {
+    await this.send("welcome", "Welcome to the Natours Family!");
+  }
 
-	async sendPasswordReset() {
-		await this.send(
-			'passwordReset',
-			'Your password reset token (valid for only 10 minutes)',
-		);
-	}
+  async sendPasswordReset() {
+    await this.send(
+      "passwordReset",
+      "Your password reset token (valid for only 10 minutes)"
+    );
+  }
 };
