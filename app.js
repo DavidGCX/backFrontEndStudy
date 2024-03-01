c62bc0a8474a09a5bdbd752c169486098a934d6d
DGCX <c62gao@uwaterloo.ca>
2024-03-01 11:38:08 -0500

    CORS set up

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const xss = require("xss-clean");
 const hpp = require("hpp");
 const cookieParser = require("cookie-parser");
 const compression = require("compression");
-
+const cors = require("cors");
 const AppError = require("./utils/appError");
 const globalErrorHandler = require("./controllers/errorController");
 
@@ -19,6 +19,13 @@ app.enable("trust proxy");
 app.set("view engine", "pug");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.static(path.join(__dirname, "public")));
+
+// Set Access-Control-Allow-Origin *
+app.use(cors());
+//app.use(cors({ origin: "http://www.example.com" }));
+
+app.options("*", cors());
+// app.options("/api/v1/tours/:id", cors());
 
 //set security HTTP headers
 app.use(helmet());
